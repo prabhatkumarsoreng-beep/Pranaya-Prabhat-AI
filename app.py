81ba5252f2db314676d74682a48e8b81d3890f2f
prabhatkumarsoreng-beep <prabhatkumarsoreng@gmail.com>
2026-03-14 14:55:45 +0530

    Create aap.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import google.generativeai as genai
 
-API_KEY = "AIzaSyDmuDEkXrSo_FsM1GTse1pslnYLmVh-rUM"
+# API KEY
+genai.configure(api_key="AIzaSyDmuDEkXrSo_FsM1GTse1pslnYLmVh-rUM")
 
-genai.configure(api_key=API_KEY)
-
+# Page setup
 st.set_page_config(page_title="Pranaya Prabhat AI", page_icon="👸")
 
 st.title("👸 Pranaya Prabhat AI")
 st.write("नमस्ते प्रभात सर जी! मैं आपकी सेवा में हाज़िर हूँ।")
 
+# Chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -17,17 +18,17 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# Chat input
 prompt = st.chat_input("यहाँ लिखें...")
 
 if prompt:
-
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("user"):
         st.write(prompt)
 
     try:
-        model = genai.GenerativeModel("models/gemini-1.5-flash")
+        model = genai.GenerativeModel("models/gemini-1.5-pro")
 
         response = model.generate_content(prompt)
 
@@ -39,4 +40,4 @@ if prompt:
         st.session_state.messages.append({"role": "assistant", "content": answer})
 
     except Exception as e:
-        st.error(f"Error आया है: {e}")
+        st.error(f"Error: {e}")
